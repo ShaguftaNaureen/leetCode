026b39308b7a1a9c00ffc3fb1fdb9c9f25a605b5
ShaguftaNaureen <49707517+ShaguftaNaureen@users.noreply.github.com>
2024-05-27 23:20:14 +0530

    Time: 56 ms (31.34%), Space: 48.5 MB (90.09%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let left =0, right = nums.length -1
+    let left=0, right=nums.length-1
 
-    while(left <= right){
+    while(left<=right){
         let mid = Math.floor((left+right)/2)
 
-        let lval = (mid==0) ? -Infinity : nums[mid-1]
-        let rval = (mid==nums.length-1) ? -Infinity : nums[mid+1]
+        let lval = (mid == 0)? -Infinity : nums[mid-1]
+        let rval = (mid == nums.length-1)? -Infinity : nums[mid+1]
 
         if(nums[mid]>lval && nums[mid]>rval){
             return mid
@@ -19,4 +19,6 @@ var findPeakElement = function(nums) {
             left = mid+1
         }
     }
+
+    return 0
 };
