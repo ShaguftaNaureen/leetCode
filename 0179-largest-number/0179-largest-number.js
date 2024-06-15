aed307f8e10c583008f80f92dbaa936706c166df
ShaguftaNaureen <49707517+ShaguftaNaureen@users.noreply.github.com>
2024-06-15 09:43:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0179-largest-number/0179-largest-number.js
+++ 0179-largest-number/0179-largest-number.js
@@ -2,13 +2,13 @@
  * @param {number[]} nums
  * @return {string}
  */
-// var largestNumber = function(nums) {
-//     return nums.sort((a,b)=>{
-//         return (b+''+a)- (a+''+b)
-//     }).join('').replace(/^0*/,'') || 0
-// };
 var largestNumber = function(nums) {
-  return nums.sort(function(a, b) {
-        return (b + '' + a ) - (a + '' + b);
-    }).join('').replace(/^0*/,'') || '0';
-}
+    return nums.sort((a,b)=>{
+        return (b+''+a)- (a+''+b)
+    }).join('').replace(/^0*/,'') || 0
+};
+// var largestNumber = function(nums) {
+//   return nums.sort(function(a, b) {
+//         return (b + '' + a ) - (a + '' + b);
+//     }).join('').replace(/^0*/,'') || '0';
+// }
