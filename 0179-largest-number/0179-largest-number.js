c85c2f82a35175427adb108e7b21e65a84e6dcb7
ShaguftaNaureen <49707517+ShaguftaNaureen@users.noreply.github.com>
2024-06-15 09:43:14 +0530

    Time: 58 ms (51.45%), Space: 50.6 MB (73.48%) - LeetHub

--- 0179-largest-number/0179-largest-number.js
+++ 0179-largest-number/0179-largest-number.js
@@ -5,7 +5,7 @@
 var largestNumber = function(nums) {
     return nums.sort((a,b)=>{
         return (b+''+a)- (a+''+b)
-    }).join('').replace(/^0*/,'') || 0
+    }).join('').replace(/^0*/,'') || '0'
 };
 // var largestNumber = function(nums) {
 //   return nums.sort(function(a, b) {
