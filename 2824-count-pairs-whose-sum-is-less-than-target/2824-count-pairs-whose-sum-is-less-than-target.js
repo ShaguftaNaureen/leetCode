b81fe7930e5519e391392c468691254c2b5c718e
ShaguftaNaureen <49707517+ShaguftaNaureen@users.noreply.github.com>
2024-06-19 09:04:03 +0530

    Time: 77 ms (8.67%), Space: 51.6 MB (12.59%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -4,15 +4,30 @@
  * @return {number}
  */
 var countPairs = function(nums, target) {
-    let count = 0
+    // let count = 0
 
-    for(let i=0; i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if(i<j && ((nums[i] + nums[j] )< target)){
-                count++
-            }
+    // for(let i=0; i<nums.length; i++){
+    //     for(let j=i+1; j<nums.length; j++){
+    //         if(i<j && ((nums[i] + nums[j] )< target)){
+    //             count++
+    //         }
+    //     }
+    // }
+
+    // return count
+
+    nums.sort((a,b)=> a - b)
+
+    let left=0, right = nums.length-1, ans = 0
+
+    while(left<right){
+        if(nums[left]+ nums[right] < target){
+            ans += right-left
+            left+=1
+        }else{
+            right-=1
         }
     }
 
-    return count
+    return ans
 };
